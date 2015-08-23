9b9dccb62cd7267b32b22e249034273bd57d0cf1
Renan Traba <rvt10@inf.ufpr.br>
2015-08-23 17:20:34 -0300

    simplify SQLAlchemyController.get_items

--- le_crud/controller/sqlalchemy.py
+++ le_crud/controller/sqlalchemy.py
@@ -1,6 +1,4 @@
-import operator
-import functools
-
+from sqlalchemy import and_, or_
 from .base import Controller
 
 
@@ -25,29 +23,37 @@ class SQLAlchemyController(Controller):
         self.db_session.delete(model)
         self.db_session.commit()
 
-    def filter_by(self, filters, op=operator.and_):
-        return functools.reduce(op, filters)
-
     def get_items(self,
                   page=1, order_by=None, filters=None, search=None):
         """
-        Return a paginated list of columns.
-        order_by will be used in a SQLAlchemy order_by.
-        filters is a iterable with 2-items tuple of field, value.
-        search is similar with filters, but use a OR condition instead.
+        Fetch database for items matching.
+
+        Args:
+            page (int):
+                which page will be sliced
+                slice size is ``self.per_page``
+            order_by (InstrumentedAttribute):
+                a field used to order query
+            filters (Iterable):
+                a iterable with sqlalchemy Expressions
+                will be merged with sqlalchemy.and_
+            search (Iterable):
+                a iterable with sqlalchemy Expressions
+                will be merged with sqlalchemy.or_
+
+        Returns:
+            tuple with:
+                items, sliced by page*self.per_page
+                total items without slice
         """
         start, end = (page-1)*self.per_page, (page)*self.per_page
         query = self.get_query()
-        if None not in (search, self.search_fields):
-            query = query.filter(self.filter_by([
-                (field == search) for field in self.search_fields
-            ], op=operator.or_))
-        if filters is not None:
-            query = query.filter(self.filter_by([
-                (field == value) for field, value in filters
-            ]))
         if order_by is not None:
             query = query.order_by(order_by)
+        if filters is not None:
+            query = query.filter(and_(*filters))
+        if search is not None:
+            query = query.filter(or_(*search))
         return query.offset(start).limit(end), query.count()
 
     def get_item(self, pk):
