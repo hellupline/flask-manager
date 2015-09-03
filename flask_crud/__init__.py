98725b702d842f39aa82bc190a6481cbd74455a2
Renan Traba <rvt10@inf.ufpr.br>
2015-09-03 15:57:09 -0300

    set version

---
+++ flask_crud/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.0.1'
+__author__ = 'Renan Vedovato Traba'
+__email__ = 'hellupline@gmail.com'
+
+from .crud import Group, Crud
+from .components import List, Create, Read, Update, Delete
