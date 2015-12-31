510c7e88437e7e344dd1774168ea7ddc6e30edc6
Renan Traba <rvt10@inf.ufpr.br>
2015-12-31 04:18:39 -0200

    simplify get_roles interface

--- flask_crud/ext/restricted_level.py
+++ flask_crud/ext/restricted_level.py
@@ -4,9 +4,10 @@ from flask_login import current_user, login_required
 class RestrictedMixin:
     @login_required
     def get_roles(self):
+        # list(map(operator.itergetter(1), root))[1:] -> all roles
         user_roles = current_user.get_roles()
         roles = super().get_roles()
         return {
-            key: list(set(values) & set(user_roles[key]))
+            key: list(set(values) & set(user_roles))
             for key, values in roles.items()
         }
