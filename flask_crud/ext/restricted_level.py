910884072246dc021b1688c34f73d296ae931ac8
Renan Traba <rvt10@inf.ufpr.br>
2016-01-01 00:26:08 -0200

    login_required decorator works better in views

--- flask_crud/ext/restricted_level.py
+++ flask_crud/ext/restricted_level.py
@@ -1,8 +1,36 @@
 from flask_login import current_user, login_required
+from flask_crud.components import List, Create, Read, Update, Delete
+
+
+class RestrictedList(List):
+    decorators = [login_required]
+
+
+class RestrictedCreate(Create):
+    decorators = [login_required]
+
+
+class RestrictedRead(Read):
+    decorators = [login_required]
+
+
+class RestrictedUpdate(Update):
+    decorators = [login_required]
+
+
+class RestrictedDelete(Delete):
+    decorators = [login_required]
 
 
 class RestrictedMixin:
-    @login_required
+    components = (
+        RestrictedList,
+        RestrictedCreate,
+        RestrictedRead,
+        RestrictedUpdate,
+        RestrictedDelete,
+    )
+
     def get_roles(self):
         # list(map(operator.itergetter(1), root))[1:] -> all roles
         user_roles = current_user.get_roles()
