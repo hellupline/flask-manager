10578a07cdb8a6bdb0301d545dad7a46454a6bf6
Renan Traba <rvt10@inf.ufpr.br>
2016-01-04 00:22:06 -0200

    hide menu from users without role

--- flask_crud/ext/restricted_level.py
+++ flask_crud/ext/restricted_level.py
@@ -1,11 +1,27 @@
 from flask_login import current_user, login_required
+from flask_crud import Group
+
+
+class RestrictedGroup(Group):
+    def endpoints(self):
+        user_roles = current_user.get_roles()
+        endpoints = super().endpoints()
+        return self._filter(endpoints, user_roles)
+
+    def _filter(self, menu, user_roles):
+        try:
+            name, endpoint, children = menu
+        except TypeError:
+            return
+        children = [self.filter_menu(child, user_roles) for child in children]
+        if endpoint.strip('.') in user_roles:
+            return name, endpoint, list(filter(None.__ne__, children))
 
 
 class RestrictedMixin:
     decorators = [login_required]
 
     def get_roles(self):
-        # list(map(operator.itergetter(1), root))[1:] -> all roles
         user_roles = current_user.get_roles()
         roles = super().get_roles()
         return {
