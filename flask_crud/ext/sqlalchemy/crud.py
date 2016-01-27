8572b3b3bea8f481f244f0ba440c286e988b241e
Renan Traba <rvt10@inf.ufpr.br>
2016-01-27 14:43:55 -0200

    create only simple model forms, custom forms should be create outside

--- flask_crud/ext/sqlalchemy/crud.py
+++ flask_crud/ext/sqlalchemy/crud.py
@@ -1,7 +1,21 @@
 from cached_property import cached_property
+from wtforms_alchemy import ModelForm
+import sqlalchemy as sa
 
 from flask_crud import crud, rules as rules_
-from flask_crud.ext.sqlalchemy import utils, controller as controller_
+from flask_crud.ext.sqlalchemy import controller as controller_
+
+
+def get_model_name(model_class):
+    return sa.inspect(model_class).entity.__name__
+
+
+def get_columns(model_class):
+    columns = sa.inspect(model_class).columns
+    return [
+        key for key, column in columns.items()
+        if not (column.primary_key or column.foreign_keys)
+    ]
 
 
 class SQLAlchemyCrud(crud.Crud):
@@ -11,9 +25,9 @@ class SQLAlchemyCrud(crud.Crud):
     actions = None
     filters = None
 
-    @cached_property
-    def name(self):
-        return utils.get_model_name(self.model)
+    def __init__(self, *args, **kwargs):
+        self.name = get_model_name(self.model)
+        super().__init__(*args, **kwargs)
 
     @cached_property
     def controller(self):
@@ -24,12 +38,14 @@ class SQLAlchemyCrud(crud.Crud):
 
     @cached_property
     def form_class(self):
-        return utils.build_form(
-            db_session=self.db_session, model_class=self.model)
+        class Form(ModelForm):
+            class Meta:
+                model = self.model
+        return Form
 
     @cached_property
     def rules(self):
-        model_columns = utils.get_columns(self.model)
+        model_columns = get_columns(self.model)
 
         columns_rules = rules_.ColumnSet(model_columns)
         data_rules = rules_.DataFieldSet(model_columns)
