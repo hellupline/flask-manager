4ea806f4fa1e84b2593a3199092ef3026c7e0e09
Renan Traba <hellupline@gmail.com>
2016-07-12 15:52:12 -0300

    added "toggle-all"

--- flask_manager/static/foundation/js/app.js
+++ flask_manager/static/foundation/js/app.js
@@ -2,11 +2,18 @@
     'use strict';
     $(document).foundation();
 
-    $(".input-select-link").on('click', function() {
+    $('.input-select-link').on('click', function() {
         var self = $(this);
         var field = $('#'+self.data('field'));
         var value = self.data('value');
         field.val(value);
         field[0].form.submit();
     });
+
+    $('#select-all').on('click', function() {
+        var checked = document.getElementById('select-all').checked;
+        for (let checkbox of document.getElementsByName('ids')) {
+            checkbox.checked = checked;
+        }
+    })
 }());
